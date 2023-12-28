61001202e0ac89f49aa2e683fc1fb7f7f560f222
Bikky Kc <bharathkc013@gmail.com>
2023-12-28 07:02:28 +0545

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,17 @@
 const { cartValiate } = require("../validators/cartValidator");
 const { addToCart, getCart } = require("../models/courseModel");
 
+
+
+const getCartData = async (req, res, next) => {
+  try {
+    const cart = await getCart();
+    res.json(cart);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const addCart = async (req, res, next) => {
   try {
     const cartData = await cartValiate.validateAsync(req.body);
@@ -14,13 +25,5 @@ const addCart = async (req, res, next) => {
   }
 };
 
-const getCartData = async (req, res, next) => {
-  try {
-    const cart = await getCart();
-    res.json(cart);
-  } catch (error) {
-    next(error);
-  }
-};
 
 module.exports = { addCart, getCartData };
